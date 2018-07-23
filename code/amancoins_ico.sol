516c17abeb613e11e8a99763ae18a22f8ae929f7
Aman Ali <pamanalionline@gmail.com>
2018-07-23 01:16:08 -0400

    Update amancoins_ico.sol

--- code/amancoins_ico.sol
+++ code/amancoins_ico.sol
@@ -2,3 +2,17 @@
 
 //version of compiler
 pragma solidity ^0.4.11;
+contract amancoin_ico { //contract defination
+
+    //Setting the total number of amancoins available for offering
+    uint public max_amancoins = 1000000 ;//unsigned intger
+                                        //Public variable, everyone will know its value
+                                        //initialised to one million
+    
+    //Introducing USD to amancoins conversion rate
+    uint public usd_to_amancoins = 1000;
+
+    // introducing the total number of amancoins that have been bought by investors
+    uint public total_amancoins_bought = 0;
+    
+}
